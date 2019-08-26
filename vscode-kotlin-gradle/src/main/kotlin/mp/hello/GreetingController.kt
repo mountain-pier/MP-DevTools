1d2898a7f887f5b541d30b8013769da9e2b7f649
RookieXie <540262228@qq.com>
2019-08-26 18:29:22 +0800

    ':sparkles:springboot'

---
+++ vscode-kotlin-gradle/src/main/kotlin/mp/hello/GreetingController.kt
@@ -0,0 +1,12 @@
+package mp.hello
+
+@RestController
+class GreetingController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+           "hhh"
+
+}
